4acce139dd779b490718c217b11de09719bf7b2f
ThuanVu <thuanvd.uit@gmail.com>
2019-04-18 00:52:26 +0700

    ok

--- src/Demo.js
+++ src/Demo.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 
+const eml = "Hello world";
+
+function merge_name(user){
+    return user.firstname + ' ' +user.lastname;
+}
+
+const user = {
+    firstname : "Thuận",
+    lastname : "Vũ"
+}
+
+function timedate() {
+    return new Date().toLocaleTimeString();
+}
+
+function Welcome(name) {
+    return <h1>Hello, {name.lastname}</h1>
+}
+
+setInterval(timedate, 1000);
 class Demo extends Component {
   render() {
     return (
      <div>
-       <h1>Hi</h1>
-       <p>
-         Hello world
-       </p>
+       <Welcome lastname="thuan" />
+       <Welcome lastname="thuan" />
+       <Welcome lastname="thuan" />
      </div>
     );
   }
 }
-
 export default Demo;
