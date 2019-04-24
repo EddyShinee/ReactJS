fe92c1002cc0e6f65a527fb70ff31108882f52eb
Thuan Vu <thuanvd.uit@gmail.com>
2019-04-24 15:07:24 +0700

    okie

--- src/components/layouts/header/Header.js
+++ src/components/layouts/header/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Logo from './Logo';
 
 class Header extends Component{
     render()
     {
         return(
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#"><Logo></Logo></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
---
+++ src/components/layouts/header/Logo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Logo extends Component{
+    render()
+    {
+        return (
+            <img src="https://tbncdn.freelogodesign.org/36d10b7c-d6a0-4b2e-8a25-a3b28b68417f.png"  alt="ABC" />
+        );
+    }
+}
+export default Logo;
