2d274b7cc2ca4d16b987ef39ce961263897fba25
Thuan Vu <thuanvd.uit@gmail.com>
2019-05-16 17:27:12 +0700

    okie

--- src/components/layouts/content/Product.js
+++ src/components/layouts/content/Product.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class Product extends Component{
+    constructor(props) {
+        super(props);
+        this.addToCard = this.addToCard.bind(this);
+    }
+    addToCard() {
+        alert("Mua thành công sản phẩm: " + this.props.name);
+        console.log(1);
+        this.test('abc', this.addToFavo);
+    }
+        addToFavo = () => {
+            alert("Mua thành công sản phẩm: " + this.props.name);
+        }
+
+        test(a,b){
+            console.log('test');
+        }
+
     render()
     {
         return(
             <div className="border">
-
                 <div>
                     <img src={this.props.image} alt={this.props.name} />
                 </div>
@@ -20,10 +36,8 @@ class Product extends Component{
                     </b>
                 </div>
                 <div>
-                    <button type="button" className="btn btn-primary">Mua ngay</button>
-
-                    <button type="button" className="btn btn-info">Giỏ hàng</button>
-
+                    <button type="button" className="btn btn-primary" onClick={this.addToCard}>Mua ngay</button>
+                    <button type="button" className="btn btn-info" onClick={this.addToFavo}>Giỏ hàng</button>
                 </div>
             </div>
         );
