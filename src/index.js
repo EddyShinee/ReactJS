3ae8f9d89ee636d38fc71c339071b90acc0f28ad
Eddy Shinee <thuanvd.uit@gmail.com>
2019-05-22 22:57:08 +0700

    22/05/2019

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import Header from './components/layouts/header/Header';
 import * as serviceWorker from './serviceWorker';
 import Content from "./components/layouts/content/Content";
 import Footer from "./components/layouts/footer/Footer";
-
+import Register from "./components/layouts/content/forms/Register";
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<Content />, document.getElementById('content'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
+ReactDOM.render(<Register />, document.getElementById('register'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
