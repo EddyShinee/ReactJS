ba80d07872546e5083bafb384a9db7f661c4a698
ThuanVu <thuanvd.uit@gmail.com>
2019-11-17 13:59:25 +0700

    Revert "Merge remote-tracking branch 'origin/master' into REACT-1904-0017"
    This reverts commit a1223b88c6be8b2859652bef3e9bea46a94d53ba.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Demo from './Demo.js';
-import Button from '@material-ui/core/Button';
-
 
 class App extends Component {
   render() {
@@ -11,11 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -24,7 +19,6 @@ class App extends Component {
           >
             Learn React
           </a>
-            <p className="Demo"/>
         </header>
       </div>
     );
--- src/Demo.js
+++ src/Demo.js
@@ -1,53 +1,16 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Button from '@material-ui/core/Button';
 
 class Demo extends Component {
-    onClick() {
-        console.log("Appp ");
-    }
-    render() {
-        var products = [
-            {
-                id: 1,
-                name: 'Iphone 7 Plus 16GB - Màu đen - Màu đỏ',
-                price: '15.000.000',
-                image: 'https://cdn.tgdd.vn/Products/Images/42/78124/iphone-7-plus-gold-400x460.png'
-            },
-            {
-                id: 2,
-                name: 'Samsung Galaxy S8 - 128GB',
-                price: '17.000.000',
-                image: 'https://cdn.tgdd.vn/Products/Images/42/190321/iphone-xs-max-gray-400x460.png'
-            },
-            {
-                id: 3,
-                name: 'Oppo F11S - 64GB',
-                price: '12.000.000',
-                image: 'https://cdn.tgdd.vn/Products/Images/42/199041/vivo-v15-quanghai-400x460.png'
-            }
-        ];
-
-        let elements = products.map((product, index) => {
-            return(
-                <div className='col-4'>
-                    <Header key={index} name={product.name} price={product.price} image={product.image}/>
-                    <Button variant="contained" color="primary">
-                        Click me man
-                    </Button>
-                </div>
-            )});
-        return (
-            <div className='row'>
-                { elements }
-                <div>
-                    <button type="button" className="btn btn-warning" onClick={this.onClick}>Click Me</button>
-                </div>
-            </div>
-
-        );
-    }
+  render() {
+    return (
+     <div>
+       <h1>Hi</h1>
+       <p>
+         Hello world
+       </p>
+     </div>
+    );
+  }
 }
 
 export default Demo;
